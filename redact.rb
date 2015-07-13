80a3d5015d34d5de46b69edf27adb28ba5ee3581
Denys Triasunov <dendden@users.noreply.github.com>
2015-07-13 13:47:52 +0300

    version 1.0
    This version only redacts 1 chosen word in case words are only
    separated by whitespaces. The redacted result is printed out, but not
    stored in a separate string.

---
+++ redact.rb
@@ -0,0 +1,16 @@
+puts "Text:"
+text = gets.chomp
+
+puts "Word to redact:"
+redact = gets.chomp
+
+words = text.split(" ")
+
+words.each do |word|
+    if word == redact
+        print "REDACTED "
+    else
+        print word + " "
+    end
+end
+
